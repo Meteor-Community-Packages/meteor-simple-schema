eb5542984ad741f569214dec46bdb982ca9f55ad
Eric Dobbertin <eric@dairystatedesigns.com>
2013-08-17 11:48:55 -0500

    compatibility updates for Meteor 0.6.5

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  summary: "A simple schema validation object with reactivity. Used by collection2."
+  summary: "A simple schema validation object with reactivity. Used by collection2 and autoform."
 });
 
 Package.on_use(function(api) {
   api.use(['deps', 'underscore'], ['client', 'server']);
   api.add_files(['simple-schema.js'], ['client', 'server']);
+  api.export(['SimpleSchema', 'SchemaRegEx', 'check'], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
     api.use(["meteor-simple-schema", "tinytest", "test-helpers"]);
     api.add_files("simple-schema-tests.js", ['client', 'server']);
-    api.export(['SimpleSchema', 'SchemaRegEx'], ['client', 'server']);
 });
--- simple-schema.js
+++ simple-schema.js
@@ -1,13 +1,13 @@
 //URL RegEx from https://gist.github.com/dperini/729294
 //http://mathiasbynens.be/demo/url-regex
 
-//@export SchemaRegEx
+//exported
 SchemaRegEx = {
     Email: /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/,
     Url: /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i
 };
 
-//@export SimpleSchema
+//exported
 SimpleSchema = function(schema) {
     var self = this;
     self._schema = schema || {};
@@ -22,7 +22,7 @@ SimpleSchema = function(schema) {
 };
 
 var builtInCheck = check;
-//@export check
+//exported
 check = function(/*arguments*/) {
     var args = _.toArray(arguments);
     if (args && _.isObject(args[0]) && args[1] instanceof SimpleSchema) {
