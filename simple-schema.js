41b369cd8987b090b5821ffc451400dd333c650f
Michael Gallagher <mjgallag@gmail.com>
2016-02-24 13:27:28 -0800

    Sync Messages with RegEx
    1. Remove Message for WeakEmail RegEx
    2. Add Message for ZipCode RegEx
    3. Add Message for Phone RegEx

--- simple-schema.js
+++ simple-schema.js
@@ -930,14 +930,15 @@ SimpleSchema._globalMessages = {
   regEx: [
     {msg: "[label] failed regular expression validation"},
     {exp: SimpleSchema.RegEx.Email, msg: "[label] must be a valid e-mail address"},
-    {exp: SimpleSchema.RegEx.WeakEmail, msg: "[label] must be a valid e-mail address"},
     {exp: SimpleSchema.RegEx.Domain, msg: "[label] must be a valid domain"},
     {exp: SimpleSchema.RegEx.WeakDomain, msg: "[label] must be a valid domain"},
     {exp: SimpleSchema.RegEx.IP, msg: "[label] must be a valid IPv4 or IPv6 address"},
     {exp: SimpleSchema.RegEx.IPv4, msg: "[label] must be a valid IPv4 address"},
     {exp: SimpleSchema.RegEx.IPv6, msg: "[label] must be a valid IPv6 address"},
     {exp: SimpleSchema.RegEx.Url, msg: "[label] must be a valid URL"},
-    {exp: SimpleSchema.RegEx.Id, msg: "[label] must be a valid alphanumeric ID"}
+    {exp: SimpleSchema.RegEx.Id, msg: "[label] must be a valid alphanumeric ID"},
+    {exp: SimpleSchema.RegEx.ZipCode, msg: "[label] must be a valid zip code"},
+    {exp: SimpleSchema.RegEx.Phone, msg: "[label] must be a valid phone number"}
   ],
   keyNotInSchema: "[key] is not allowed by the schema"
 };
