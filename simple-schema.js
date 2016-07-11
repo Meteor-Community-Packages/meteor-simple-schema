8fc42f05f0117758dc106f9ef0c25ac35b7396ee
Data-Meister <Data-Meister@users.noreply.github.com>
2016-07-11 17:07:07 +0100

    Add a validateOne method to SimpleSchema.prototype

--- simple-schema.js
+++ simple-schema.js
@@ -1174,6 +1174,38 @@ SimpleSchema.prototype.validate = function (obj, options) {
   throw new Package['mdg:validation-error'].ValidationError(errors, message);
 };
 
+
+
+SimpleSchema.prototype.validateOne = function (obj, key, options) {
+  if (Package.check && Package['audit-argument-checks']) {
+    // Call check but ignore the error to silence audit-argument-checks
+    try { check(obj); } catch (e) { /* ignore error */ }
+  }
+
+  var validationContext = this.newContext();
+  var isValid = validationContext.validateOne(obj, key, options);
+
+  if (isValid) return;
+
+  var errors = validationContext.invalidKeys().map(function (error) {
+    return {
+      name: error.name,
+      type: error.type,
+      details: {
+        value: error.value
+      }
+    };
+  });
+
+  // In order for the message at the top of the stack trace to be useful,
+  // we set it to the first validation error message.
+  var message = validationContext.keyErrorMessage(errors[0].name);
+
+  throw new Package['mdg:validation-error'].ValidationError(errors, message);
+};
+
+
+
 SimpleSchema.prototype.validator = function (options) {
   var self = this;
   options = options || {};
