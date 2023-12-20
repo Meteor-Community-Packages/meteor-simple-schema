cfa3cd42b1a59913fba4db7069126f1f0197e7d3
jankapunkt <jkuester@uni-bremen.de>
2023-12-20 09:11:52 +0100

    refactor: removed old files

--- package.js
+++ package.js
@@ -1,43 +1,31 @@
+/* eslint-env meteor */
 Package.describe({
   name: 'aldeed:simple-schema',
   summary: 'A simple schema validation object with reactivity. Used by collection2 and autoform.',
-  version: '1.5.4',
+  version: '2.0.0',
   git: 'https://github.com/aldeed/meteor-simple-schema.git'
 });
 
+Npm.depends({
+  'mongo-object': '3.0.1',
+  'message-box': '0.2.7',
+  'clone': '2.1.2'
+})
+
 Package.onUse(function (api) {
-  api.use('tracker@1.0.0');
-  api.use('underscore@1.0.0');
-  api.use('check@1.0.0');
-  api.use('random@1.0.0');
-
-  api.use('mdg:validation-error@0.2.0', {unordered: true});
-
-  api.addFiles([
-    'string-polyfills.js',
-    'string-humanize.js',
-    'mongo-object.js',
-    'simple-schema-utility.js',
-    'simple-schema.js',
-    'simple-schema-validation.js',
-    'simple-schema-validation-new.js',
-    'simple-schema-context.js',
-  ]);
-  api.export(['SimpleSchema', 'MongoObject'], ['client', 'server']);
-  api.export('humanize', {testOnly: true});
+  api.versionsFrom(['1.3', '2.3', '3.0-alpha.19'])
+  api.use('ecmascript')
+  api.mainModule('lib/main.js')
 });
 
 Package.onTest(function (api) {
-  api.use('aldeed:simple-schema');
-  api.use('tinytest@1.0.0');
-  api.use('test-helpers@1.0.0');
-  api.use('underscore@1.0.0');
-  api.use('check@1.0.0');
-
-  api.addFiles([
-    'simple-schema-tests.js',
-    'mongo-object-tests.js',
-    'humanize-tests.js',
-    'validation-error-tests.js'
-  ], ['client', 'server']);
-});
+  api.use([
+    'ecmascript',
+    'tracker',
+    'meteortesting:browser-tests@1.5.3',
+    'meteortesting:mocha@2.1.0',
+    'mongo',
+    'aldeed:simple-schema'
+  ])
+  api.mainModule('lib/main.tests.js')
+})
