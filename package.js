e113bba134e4df2622a9308e0b80772af18eba63
jankapunkt <jkuester@uni-bremen.de>
2023-12-21 09:57:38 +0100

    initiate migration

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'aldeed:simple-schema',
   summary: 'A simple schema validation object with reactivity. Used by collection2 and autoform.',
-  version: '1.13.1',
+  version: '2.0.0-rc.0',
   git: 'https://github.com/aldeed/meteor-simple-schema.git'
 });
 
