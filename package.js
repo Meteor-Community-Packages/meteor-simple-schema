121147d48c807192bb68008c3333e0c56f728478
ladmin <thumptech@gmail.com>
2026-01-21 10:51:02 +0000

    working through tests

--- package.js
+++ package.js
@@ -10,6 +10,7 @@ Npm.depends({
     'mongo-object': '3.0.1',
     'message-box': '0.2.7',
     'chai': '6.2.2',
+    'chai-as-promised': '8.0.2',
     clone: '2.1.2'
 })
 
@@ -32,7 +33,7 @@ Package.onTest(function (api) {
     ])
 
     api.addFiles([
-         //   'lib/clean/autoValue.tests.js', //TODO
+        //    'lib/clean/autoValue.tests.js', //TODO
         //  'lib/clean/convertToProperType.tests.js',//PASS
         //  'lib/clean/defaultValue.tests.js',//PASS
         //  'lib/clean/setAutoValues.tests.js',//PASS
@@ -40,26 +41,26 @@ Package.onTest(function (api) {
         //  'lib/clean.tests.js', //PASS
         //  'lib/expandShorthand.tests.js', //PASS
         //  'lib/humanize.tests.js', //PASS
-          'lib/SimpleSchema.tests.js', //MAJOR PROBLEMS
-        //  'lib/SimpleSchema_allowedValues.tests.js',
-        //  'lib/SimpleSchema_autoValueFunctions.tests.js',
-        //  'lib/SimpleSchema_blackbox.tests.js',
-        //  'lib/SimpleSchema_custom.tests.js',
-        //  'lib/SimpleSchema_definition.tests.js',
-        //  'lib/SimpleSchema_extend.tests.js',
-        //  'lib/SimpleSchema_getObjectSchema.tests.js',
-        //  'lib/SimpleSchema_getQuickTypeForKey.tests.js',
-        //  'lib/SimpleSchema_labels.tests.js',
-        //  'lib/SimpleSchema_max.tests.js',
-        //  'lib/SimpleSchema_messages.tests.js',
-        //  'lib/SimpleSchema_min.tests.js',
-        //  'lib/SimpleSchema_minCount.tests.js',
-        //  'lib/SimpleSchema_namedContext.tests.js',
-        //  'lib/SimpleSchema_omit.tests.js',
-        //  'lib/SimpleSchema_oneOf.tests.js',
-        //  'lib/SimpleSchema_pick.tests.js',
-        //  'lib/SimpleSchema_regEx.tests.js',
-        //  'lib/SimpleSchema_required.tests.js',
+        //  'lib/SimpleSchema.tests.js', //PASS, CHECK TODO
+        //  'lib/SimpleSchema_allowedValues.tests.js', //PASS
+        //  'lib/SimpleSchema_autoValueFunctions.tests.js'//PASS
+        //  'lib/SimpleSchema_blackbox.tests.js', //PASS
+        //  'lib/SimpleSchema_custom.tests.js', //PASS TODO: make sure custom async validation works - add new test
+        //  'lib/SimpleSchema_definition.tests.js', //PASS
+        //  'lib/SimpleSchema_extend.tests.js', //PASS
+        //  'lib/SimpleSchema_getObjectSchema.tests.js', //PASS
+        //  'lib/SimpleSchema_getQuickTypeForKey.tests.js', //PASS
+        //  'lib/SimpleSchema_labels.tests.js', //PASS
+        //  'lib/SimpleSchema_max.tests.js', //PASS
+        //  'lib/SimpleSchema_messages.tests.js', //PASS
+        //  'lib/SimpleSchema_min.tests.js', //PASS
+        //  'lib/SimpleSchema_minCount.tests.js', //PASS
+        //  'lib/SimpleSchema_namedContext.tests.js', //PASS
+        //  'lib/SimpleSchema_omit.tests.js', //PASS
+        //  'lib/SimpleSchema_oneOf.tests.js', //PASS
+        //  'lib/SimpleSchema_pick.tests.js', //PASS
+        //  'lib/SimpleSchema_regEx.tests.js', //PASS
+          'lib/SimpleSchema_required.tests.js',
         //  lib/SimpleSchema_rules.tests.js',
         //  'lib/SimpleSchema_type.tests.js',
         //  'lib/reactivity.tests.js'
