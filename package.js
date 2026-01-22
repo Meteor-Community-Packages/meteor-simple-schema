d1641df93192ec1a08d0c57d529b91025b80d8a0
ladmin <thumptech@gmail.com>
2026-01-22 00:22:42 +0000

    all tests enabled and passing

--- package.js
+++ package.js
@@ -33,36 +33,36 @@ Package.onTest(function (api) {
     ])
 
     api.addFiles([
-        //    'lib/clean/autoValue.tests.js', //TODO
-        //  'lib/clean/convertToProperType.tests.js',//PASS
-        //  'lib/clean/defaultValue.tests.js',//PASS
-        //  'lib/clean/setAutoValues.tests.js',//PASS
-        //  'lib/utility/getLastPartOfKey.tests.js',//PASS
-        //  'lib/clean.tests.js', //PASS
-        //  'lib/expandShorthand.tests.js', //PASS
-        //  'lib/humanize.tests.js', //PASS
-        //  'lib/SimpleSchema.tests.js', //PASS, CHECK TODO
-        //  'lib/SimpleSchema_allowedValues.tests.js', //PASS
-        //  'lib/SimpleSchema_autoValueFunctions.tests.js'//PASS
-        //  'lib/SimpleSchema_blackbox.tests.js', //PASS
-        //  'lib/SimpleSchema_custom.tests.js', //PASS TODO: make sure custom async validation works - add new test
-        //  'lib/SimpleSchema_definition.tests.js', //PASS
-        //  'lib/SimpleSchema_extend.tests.js', //PASS
-        //  'lib/SimpleSchema_getObjectSchema.tests.js', //PASS
-        //  'lib/SimpleSchema_getQuickTypeForKey.tests.js', //PASS
-        //  'lib/SimpleSchema_labels.tests.js', //PASS
-        //  'lib/SimpleSchema_max.tests.js', //PASS
-        //  'lib/SimpleSchema_messages.tests.js', //PASS
-        //  'lib/SimpleSchema_min.tests.js', //PASS
-        //  'lib/SimpleSchema_minCount.tests.js', //PASS
-        //  'lib/SimpleSchema_namedContext.tests.js', //PASS
-        //  'lib/SimpleSchema_omit.tests.js', //PASS
-        //  'lib/SimpleSchema_oneOf.tests.js', //PASS
-        //  'lib/SimpleSchema_pick.tests.js', //PASS
-        //  'lib/SimpleSchema_regEx.tests.js', //PASS
-          'lib/SimpleSchema_required.tests.js',
-        //  lib/SimpleSchema_rules.tests.js',
-        //  'lib/SimpleSchema_type.tests.js',
-        //  'lib/reactivity.tests.js'
+        'lib/clean/autoValue.tests.js', //PASS TODO
+        'lib/clean/convertToProperType.tests.js',//PASS
+        'lib/clean/defaultValue.tests.js',//PASS
+        'lib/clean/setAutoValues.tests.js',//PASS
+        'lib/utility/getLastPartOfKey.tests.js',//PASS
+        'lib/clean.tests.js', //PASS
+        'lib/expandShorthand.tests.js', //PASS
+        'lib/humanize.tests.js', //PASS
+        'lib/SimpleSchema.tests.js', //PASS, CHECK TODO
+        'lib/SimpleSchema_allowedValues.tests.js', //PASS
+        'lib/SimpleSchema_autoValueFunctions.tests.js',//PASS
+        'lib/SimpleSchema_blackbox.tests.js', //PASS
+        'lib/SimpleSchema_custom.tests.js', //PASS TODO: make sure custom async validation works - add new test
+        'lib/SimpleSchema_definition.tests.js', //PASS
+        'lib/SimpleSchema_extend.tests.js', //PASS
+        'lib/SimpleSchema_getObjectSchema.tests.js', //PASS
+        'lib/SimpleSchema_getQuickTypeForKey.tests.js', //PASS
+        'lib/SimpleSchema_labels.tests.js', //PASS
+        'lib/SimpleSchema_max.tests.js', //PASS
+        'lib/SimpleSchema_messages.tests.js', //PASS
+        'lib/SimpleSchema_min.tests.js', //PASS
+        'lib/SimpleSchema_minCount.tests.js', //PASS
+        'lib/SimpleSchema_namedContext.tests.js', //PASS
+        'lib/SimpleSchema_omit.tests.js', //PASS
+        'lib/SimpleSchema_oneOf.tests.js', //PASS
+        'lib/SimpleSchema_pick.tests.js', //PASS
+        'lib/SimpleSchema_regEx.tests.js', //PASS
+        'lib/SimpleSchema_required.tests.js', //PASS
+        'lib/SimpleSchema_rules.tests.js', //PASS
+        'lib/SimpleSchema_type.tests.js', //PASS
+        'lib/reactivity.tests.js'
     ])
 })
